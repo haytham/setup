fb26041e996f48b67700ab634f0c4e54eeaed7e8
haytham <haytham.samad@gmail.com>
2013-08-28 05:43:29 +0000

    Added heroku install and changed to pull my dotfiles repot

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@
 sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
+# Install heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
@@ -34,7 +37,11 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+
+# setup my own dot files from my repo
+git clone https://github.com/haytham/dotfiles.git
+
+#git clone https://github.com/startup-class/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
